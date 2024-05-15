fb29bc25470c0a739da71d41772790f831b5bae0
Jhosban <jhosban8@gmail.com>
2024-05-15 13:41:28 -0500

    Creacion de proyecto MERN

---
+++ Node_Backend/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const MONGODB_URI = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&authSource=admin`;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log(">>> Connected Database");
+  })
+  .catch((err) => {
+    console.log(err);
+    throw err;
+  });
+
+app.get("/status", (req, res) => {
+  res.status(200).send({
+    success: "true",
+    message: "Server running",
+  });
+});
+
+
+
+module.exports = app;
